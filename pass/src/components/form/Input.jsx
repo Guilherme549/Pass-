6eafef3762e6a338071d686d42f534c01c9a96ec
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2024-04-25 20:11:14 -0300

    estilizando input de pesquisa

--- pass/src/components/form/Input.jsx
+++ pass/src/components/form/Input.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import style from './Input.module.css'
 
-export default function Input({type, name}) {
+export default function Input({type, name, id, text, placeholder, value}) {
   return (
-    <div>
+    <div className={style.search_btn}>
         <label htmlFor={name}>{text}</label>
-        <input type={type} name={name} />
+        <input type={type} name={name} id={id} placeholder={placeholder} value={value} />
     </div>
   )
 }
--- pass/src/components/pages/SearchClinic.jsx
+++ pass/src/components/pages/SearchClinic.jsx
@@ -4,10 +4,12 @@ import Input from '../form/Input'
 export default function SearchClinic() {
   return (
     <div>
-        <div>
-            <h2>Olá, Paciente</h2>
-            
-        </div>
+      <div>
+        <h2>Olá, Paciente</h2>
+        <Input type='text' name='buscarCidade' placeholder='Busque pela cidade' />
+        <Input type='text' name='atendimento' placeholder='Atendimento' />
+        <Input type='text' name='tipoAtendimento' placeholder='Tipo de atendimento' />
+      </div>
     </div>
   )
 }
