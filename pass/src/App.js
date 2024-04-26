46e79c77764ddb34a72630c37c2d68e5cf51c8e9
Kevin <kevindff@hotmail.com>
2024-04-25 23:32:15 -0300

    Register page

--- pass/src/App.js
+++ pass/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Nav from './components/layout/NavBar';
 import SearchClinic from './components/pages/SearchClinic';
-import Signup from './components/pages/Signup'
+import Register from './components/pages/Register'
 import Login from './components/pages/Login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
      <Router>
        <Routes>
-          <Route path='Signup/' element={<Signup />}></Route>
+          <Route path='register/' element={<Register />}></Route>
           <Route path='login/' element={<Login/>}></Route>
           <Route path='search/' element={<SearchClinic />}></Route>
        </Routes>
