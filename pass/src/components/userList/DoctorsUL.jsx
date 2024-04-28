67f14fd63a506eac4726e62b92421bb4ba4f298b
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2024-04-28 12:04:50 -0300

    img

--- pass/src/components/userList/DoctorsUL.jsx
+++ pass/src/components/userList/DoctorsUL.jsx
@@ -6,12 +6,26 @@ import { SlCalender } from "react-icons/sl";
 import styles from './DoctorsUL.module.css'
 import ana from '../../img/ana.png'
 import Button from "../forms/Button";
+import antonio from '../../img/image.png'
 
 export default function DoctorsUL() {
     return (
         <ul className={styles.doctorsList}>
             <li className={styles.doctorItem}>
                 <img src={ana} alt="Doctor Profile" className={styles.doctorImg} />
+                <div className={styles.doctorDetails}>
+                    <h1 className={styles.doctorName}>
+                        Dra. Ana Maria Nogueira <span><MdOutlineVerified /></span>
+                    </h1>
+                    <p className={styles.description}>Dermatologista, especialista em estética e câncer de pele</p>
+                    <p className={styles.stars}><FaRegStar /><FaRegStar /><FaRegStar /><FaRegStar /><FaRegStar /></p>
+                    <p className={styles.address}><CiMap /> Rua tal</p>
+                    <p className={styles.appointment}><SlCalender /> Dia tal</p>
+                    <Button placeholder="Agendar" />
+                </div>
+            </li>
+            <li className={styles.doctorItem}>
+                <img src={antonio} alt="Doctor Profile" className={styles.doctorImg} />
                 <div className={styles.doctorDetails}>
                     <h1 className={styles.doctorName}>
                         Dra. Ana Maria Nogueira <span><MdOutlineVerified /></span>
