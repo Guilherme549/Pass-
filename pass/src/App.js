d6cd180fc794df6e368c94a8ea59a1ca82915e36
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2024-04-28 13:30:13 -0300

    Instalando next.js

--- pass/src/App.js
+++ pass/src/App.js
@@ -4,17 +4,20 @@ import SearchClinic from './components/pages/SearchClinic';
 import Signup from './components/pages/Signup'
 import Login from './components/pages/Login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignupMedic from './components/medic_area/Signup_medic';
 
 function App() {
   return (
     <div className="App">
-     <Router>
-       <Routes>
-          <Route path='Signup/' element={<Signup />}></Route>
-          <Route path='/' element={<Login/>}></Route>
+      <Router>
+        <Routes>
+          <Route path='/signup_medic' element={<SignupMedic />}></Route>
+
+          <Route path='signup/' element={<Signup />}></Route>
+          <Route path='/' element={<Login />}></Route>
           <Route path='search/' element={<SearchClinic />}></Route>
-       </Routes>
-     </Router>
+        </Routes>
+      </Router>
     </div>
   );
 }
