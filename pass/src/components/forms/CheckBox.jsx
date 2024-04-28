3a1ee671f0efa90d912f13e48c6a09cc3bf2d14e
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2024-04-28 11:29:53 -0300

    Criando funcao do checkbox do search

--- pass/src/components/forms/CheckBox.jsx
+++ pass/src/components/forms/CheckBox.jsx
@@ -2,10 +2,24 @@ import React from "react";
 import styles from './CheckBox.module.css'
 
 export default function CheckBox({name, id, value, icon}){
+
+    function click(event){
+        if(event.target.tagName === "LABEL"){
+            const input = document.getElementById(event.target.htmlFor);
+            if(input.checked){
+                event.target.style.backgroundColor = "#f0f0f0";
+                event.target.style.color = "#626161";
+            } else {
+                event.target.style.backgroundColor = "#888888";
+                event.target.style.color = "white";
+            }
+        }
+    }
+
     return(
         <div className={styles.checkbox_filter_search}>
-            <label htmlFor={name}>{icon}{name}</label>
-            <input type="checkbox" name={name} id={id} value={value} />
+            <label onClick={click} htmlFor={name}>{icon}{name}</label>
+            <input  type="checkbox" name={name} id={id} value={value} />
         </div>
     )
 }
