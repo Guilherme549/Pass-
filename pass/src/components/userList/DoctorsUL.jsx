6901340611cce0c25e1a6917c07eccffe8a962da
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2024-05-01 13:22:57 -0300

    listando dados da API

--- pass/src/components/userList/DoctorsUL.jsx
+++ pass/src/components/userList/DoctorsUL.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import { FaRegStar } from "react-icons/fa";
 import { MdOutlineVerified } from "react-icons/md";
 import { CiMap } from "react-icons/ci";
@@ -6,70 +6,44 @@ import { SlCalender } from "react-icons/sl";
 import styles from './DoctorsUL.module.css'
 import ana from '../../img/ana.png'
 import Button from "../forms/Button";
-import antonio from '../../img/image.png'
+// Remove unused import statements
+// import antonio from '../../img/image.png'
+// import { useEffect, useState } from 'react';
 
 
 // essas li serão dinamicas
 
 export default function DoctorsUL() {
+    const [doctors, setDoctors] = useState([]);
+
+    useEffect(() => {
+        const fetchApi = async () => {
+            const url = 'http://127.0.0.1:8000/todo/lista/?format=json'
+            const response = await fetch(url);
+            const objJson = await response.json();
+            // console.log(objJson[0].name);
+            setDoctors(objJson)
+        };
+        fetchApi();
+    }, []); 
+
     return (
         <ul className={styles.doctorsList}>
-            <li className={styles.doctorItem}>
-                <img src={ana} alt="Doctor Profile" className={styles.doctorImg} />
-                <div className={styles.doctorDetails}>
-                    <h1 className={styles.doctorName}>
-                        Dra. Ana Maria Nogueira <span><MdOutlineVerified /></span>
-                    </h1>
-                    <p className={styles.description}>Dermatologista, especialista em estética e câncer de pele</p>
-                    <p className={styles.stars}><FaRegStar /><FaRegStar /><FaRegStar /><FaRegStar /><FaRegStar /></p>
-                    <p className={styles.address}><CiMap /> Rua tal</p>
-                    <p className={styles.appointment}><SlCalender /> Dia tal</p>
-                    <Button placeholder="Agendar" />
-                </div>
-            </li>
-            <li className={styles.doctorItem}>
-                <img src={antonio} alt="Doctor Profile" className={styles.doctorImg} />
-                <div className={styles.doctorDetails}>
-                    <h1 className={styles.doctorName}>
-                        Dra. Ana Maria Nogueira <span><MdOutlineVerified /></span>
-                    </h1>
-                    <p className={styles.description}>Dermatologista, especialista em estética e câncer de pele</p>
-                    <p className={styles.stars}><FaRegStar /><FaRegStar /><FaRegStar /><FaRegStar /><FaRegStar /></p>
-                    <p className={styles.address}><CiMap /> Rua tal</p>
-                    <p className={styles.appointment}><SlCalender /> Dia tal</p>
-                    <Button placeholder="Agendar" />
-                </div>
-            </li>
-            <li className={styles.doctorItem}>
-                <img src={antonio} alt="Doctor Profile" className={styles.doctorImg} />
-                <div className={styles.doctorDetails}>
-                    <h1 className={styles.doctorName}>
-                        Dra. Ana Maria Nogueira <span><MdOutlineVerified /></span>
-                    </h1>
-                    <p className={styles.description}>Dermatologista, especialista em estética e câncer de pele</p>
-                    <p className={styles.stars}><FaRegStar /><FaRegStar /><FaRegStar /><FaRegStar /><FaRegStar /></p>
-                    <p className={styles.address}><CiMap /> Rua tal</p>
-                    <p className={styles.appointment}><SlCalender /> Dia tal</p>
-                    <Button placeholder="Agendar" />
-                </div>
-            </li>
-            <li className={styles.doctorItem}>
-                <img src={antonio} alt="Doctor Profile" className={styles.doctorImg} />
-                <div className={styles.doctorDetails}>
-                    <h1 className={styles.doctorName}>
-                        Dra. Ana Maria Nogueira <span><MdOutlineVerified /></span>
-                    </h1>
-                    <p className={styles.description}>Dermatologista, especialista em estética e câncer de pele</p>
-                    <p className={styles.stars}><FaRegStar /><FaRegStar /><FaRegStar /><FaRegStar /><FaRegStar /></p>
-                    <p className={styles.address}><CiMap /> Rua tal</p>
-                    <p className={styles.appointment}><SlCalender /> Dia tal</p>
-                    <Button placeholder="Agendar" />
-                </div>
-            </li>
+            {doctors.map((e) => (
+                <li key={e.id} className={styles.doctorItem}>
+                    <img src={e.image} alt="Doctor Profile" className={styles.doctorImg} />
+                    <div className={styles.doctorDetails}>
+                        <h1 className={styles.doctorName}>
+                            {e.name} <span><MdOutlineVerified /></span>
+                        </h1>
+                        <p className={styles.description}>{e.description}</p>
+                        <p className={styles.stars}><FaRegStar /><FaRegStar /><FaRegStar /><FaRegStar /><FaRegStar /></p>
+                        <p className={styles.address}><CiMap />{e.adress}</p>
+                        <p classame={styles.appointment}><SlCalender />{e.data}</p>
+                        <Button placeholder="Agendar" />
+                    </div>
+                </li>
+            ))}
         </ul>
-        
     )
-}
-
-
-
+};
