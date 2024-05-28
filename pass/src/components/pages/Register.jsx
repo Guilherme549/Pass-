5d7230f0e112723e370c651813bab85d606b804d
Kevin Fonseca <kevindff@hotmail.com>
2024-05-28 10:34:59 -0300

    chore: Update Register page styling and add Google login functionality

--- pass/src/components/pages/Register.jsx
+++ pass/src/components/pages/Register.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import './Register.css';
 import logoPass from '../../img/logoPass.svg';
+import { signInWithGoogle } from './firabase.js'; // Importar a função de login do Google
+import { useNavigate } from 'react-router-dom';
 
 const SignupPage = () => {
+  const navigate = useNavigate();
+
+  const handleGoogleLogin = async () => {
+    try {
+      await signInWithGoogle();
+      navigate('/loggedin'); // Redirecionar após login bem-sucedido
+    } catch (error) {
+      console.error("Error signing in with Google", error);
+    }
+  };
+
   return (
     <div className="signup-page">
       <div className="signup-container">
@@ -17,6 +30,12 @@ const SignupPage = () => {
           <input type="password" placeholder="Confirme a Senha" className="signup-input" />
           <button type="submit" className="signup-button">Cadastrar</button>
         </form>
+        <div className="google-login-container">
+          <button className="google-login-button" onClick={handleGoogleLogin}>
+            <img src="https://developers.google.com/identity/images/g-logo.png" alt="Google logo" className="google-logo" />
+            Continue com Google
+          </button>
+        </div>
         <div className="login-option">
           <span>Já possui conta?</span>
           <a href="/login" className="login-link">Login</a>
